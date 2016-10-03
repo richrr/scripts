4608a9a07c3634fba349131eefca73bcce53981d
Richard Rodrigues <richrr@users.noreply.github.com>
2016-10-03 10:54:53 -0700

    added NaN as string to indicate na, removed unnecessary variables and options, arranged the comments and code for easier readability

--- R/perform-analyses.R
+++ R/perform-analyses.R
@@ -1,6 +1,27 @@
+##### to do: 
+## partial correlation
 
 
-# Rscript perform-analyses.R ./AllControl4Strains.csv --lists ./partner1.txt ./partner2.txt --mapFile mapFile.txt --mapColumns #SampleID Experiment --AnalysToDoList AnalysToDoList.txt --comparMethod mw --correlMethod sp --symbolColumnName #SampleID --genNetwork
+# Usage: Rscript perform-analyses.R ./AllControl4Strains.csv --lists ./partner1.txt ./partner2.txt --mapFile mapFile.txt --mapColumns #SampleID Experiment --AnalysToDoList AnalysToDoList.txt --comparMethod mw --correlMethod sp --symbolColumnName #SampleID --genNetwork
+
+
+# Allowed analyses (conditions): 
+# add 4th column with "correlation" to indicate whatever operation is to be done, needs to be done on correlations
+# 1. A vs B: 2 non-empty columns, last two columns empty [A	B empty empty]
+# 2. delta A vs delta B: 3 non-empty columns, 3rd column delta, 4th column empty [A	B	delta	empty]  # needs 4 groups: two comma separated from A and two comma separated from B
+# 3. A vs B paired:  3 non-empty columns, 3rd column paired, 4th column empty [A	B	paired	empty]
+# 4. Correl A: 2 non-empty columns, 4th column correlation [A	empty	empty	correlation]
+# 5. Correl delta A: 3 non-empty columns, 2nd column empty, 3rd column delta, 4th column correlation [A	empty	delta	correlation] # needs 2 groups: two comma separated from A 
+# 6. Correl A vs. Correl B: 3 non-empty columns, 4th column correlation [A	B	empty	correlation]
+# 7. Correl delta A vs. Correl delta B: 4 non-empty columns, 3rd column delta, 4th column correlation [A	B	delta	correlation] # needs 4 groups: two comma separated from A and two comma separated from B
+# 8. Correl A vs. Correl B paired: 4 non-empty columns, 3rd column paired, 4th column correlation [A	B	paired	correlation]
+## no need to do paired in 8
+# 9. Timelag Correl A and B (where A and B are different time points): 4 non-empty columns, 3rd column timelag, 4th column correlation [A	B	timelag	correlation]
+## std. correlations: g1T1-g2T1 or g1T2-g2T2
+## calculate timelag correlations for g?T1-g?T2 : will take care of g1T1-g1T2, g1T1-g2T2, etc. 
+## for all gene pairs: give group 1 (T1) to first gene, group 2 (T2) to second gene
+
+
 
 library(argparser)
 library(hash)
@@ -19,26 +40,22 @@ source("/nfs1/Morgun_Lab/richrr/scripts/R/comp-correl-delta-paired-util-functs.R
 #==================================================================================================================
 p <- arg_parser('perform-analyses.R Calculates comparison and correlation analysis between lists')
 p <- add_argument(p, "expressionDataFile", help="file containing values (measurements) to be used for correlation") # required
-p <- add_argument(p, "--lists", help="lists (of measurement labels e.g. taxa names, phenotypics tests) to generate pairs for calculate correlation", nargs=2) # required; but written as optional format so I explicitly mention the "--lists"
-p <- add_argument(p, "--output", help="output file", default="./result/network_")
 
+# truly optional
+p <- add_argument(p, "--output", help="output file", default="./result/network_")
 p <- add_argument(p, "--symbolColumnName", help="the name of the column containing the measurement labels in expression data file", default="#SampleID") # 'UniqueID'
+p <- add_argument(p, "--warnings", help="print warnings", flag=TRUE)
 
 # # required; but written as optional format so I explicitly mention the following in cmd line
-p <- add_argument(p, "--AnalysToDoList", help="AnalysToDoList", default="AnalysToDoList.txt") # tab-delimited; perform correlations or comparisons as per inmput
-p <- add_argument(p, "--mapFile", help="tab delimited mapFile", default="mapFile.txt") # use the mapping file to identify the samples group affiliation to perform correlations
-p <- add_argument(p, "--mapColumns", nargs=2, help="Columns in map file in which to search the sample group associations", default=c("#SampleID","Experiment")) # use the columns in the mapping file to identify the samples group affiliation to perform correlations
-p <- add_argument(p, "--pairedInfoColumn", help="Column in map file in which to search the mouse ids. This info will help to identify samples from same mice", default="Mouse") # Column in map file in which to search the mouse ids. This info will help to identify samples from same mice
-
-
+p <- add_argument(p, "--lists", help="lists (of measurement labels e.g. taxa names, phenotypics tests) to generate pairs for calculate correlation", nargs=2) 
+p <- add_argument(p, "--AnalysToDoList", help="AnalysToDoList", default="AnalysToDoList.txt") # tab-delimited; perform correlations and/or comparisons as per input
+p <- add_argument(p, "--mapFile", help="tab delimited mapFile", default="mapFile.txt") # use the mapping file to identify the samples group affiliation to perform analysis
+p <- add_argument(p, "--mapColumns", nargs=2, help="Columns in map file in which to search the sample group associations", default=c("#SampleID","Experiment")) # use the columns in the mapping file to identify the samples group affiliation to perform analysis
+p <- add_argument(p, "--pairedInfoColumn", help="Column in map file in which to search the mouse ids. This info will help to identify samples from same mice", default="Mouse") # Column in map file in which to search the mouse ids. This info will help to identify samples from same mice, useful when doing paired comparisons
 
 ## optional methods for t test and correlations
 p <- add_argument(p, "--comparMethod", help="method to compa A vs B", default="tt") # allowed: "tt" (Welch t test), "mw" (Mann-Whitney U Test)
 p <- add_argument(p, "--correlMethod", help="method to corre A vs B", default="pearson") # allowed: pearson, "kendall", or "spearman", can be abbreviated.
-
-
-p <- add_argument(p, "--warnings", help="print warnings", flag=TRUE)
-
 
 argv <- parse_args(p)
 #print (p)
@@ -60,11 +77,6 @@ odir =  strsplit(outputFile, "/")[[1]]
 odir = paste(odir[1:(length(odir) -1)], collapse='/')
 print(odir)
 dir.create(odir, recursive = TRUE)
-
-
-
-##### to do: 
-## partial correlation
 
 
 symbleColumnName =  argv$symbolColumnName
@@ -201,12 +213,8 @@ for(indx in 1:nrow(AnalysToDoList)){
 
 
 
-###########################################################################################################
 #	output filename
-networkFile = paste(outputFile,"individualPvalue:",individualPvalueCutoff,"_combinedPvalue:",combinedPvalueCutoff,"_combinedFDR:",combinedFDRCutoff,".csv",sep="")
 pairFile = "./testpair.txt"
-
-
 #==================================================================================================================
 # generate or load existing pairlist to avoid re-generating pairs(needs time) (for correlations and comparison of correlations)
 #==================================================================================================================
@@ -235,7 +243,7 @@ if (!file.exists(pairFile)){
 #==================================================================================================================
 # load expression data
 #==================================================================================================================
-expressionData = read.csv(expressionDataFile,header = TRUE,check.names=FALSE,na.strings=c("","na","NA"))
+expressionData = read.csv(expressionDataFile,header = TRUE,check.names=FALSE,na.strings=c("","na","NA", "NaN"))
 expressionData = expressionData[!duplicated(expressionData[,symbleColumnName]),]    #delete duplicated measurements (genes, taxa labels, phenotypic labels, etc.)
 rownames(expressionData) = expressionData[,symbleColumnName]
 
@@ -244,7 +252,7 @@ calcFC = function(v1 , v2){
     v1 = as.numeric(as.vector(v1))
     v2 = as.numeric(as.vector(v2))
 
-    FC = c() # append did not work, don't know why
+    FC = c() 
     for(i in 1:length(v1)){
         if(is.na(v1[i]) || is.na(v2[i])){
             FC = c(FC, NA)
@@ -253,8 +261,7 @@ calcFC = function(v1 , v2){
             FC = c(FC, fc)
         }
     }
-    #print(FC)
-            
+           
     return(FC)
 }
 
@@ -265,23 +272,9 @@ corrout = c()
 compout = c()
 folchout = c()
 
-# conditions: 
-# add 4th column with "correlation" to indicate whatever operation is to be done, needs to be done on correlations
-# 1. A vs B: 2 non-empty columns, last two columns empty [A	B empty empty]
-# 2. delta A vs delta B: 3 non-empty columns, 3rd column delta, 4th column empty [A	B	delta	empty]  # needs 4 groups: two comma separated from A and two comma separated from B
-# 3. A vs B paired:  3 non-empty columns, 3rd column paired, 4th column empty [A	B	paired	empty]
-# 4. Correl A: 2 non-empty columns, 4th column correlation [A	empty	empty	correlation]
-# 5. Correl delta A: 3 non-empty columns, 2nd column empty, 3rd column delta, 4th column correlation [A	empty	delta	correlation] # needs 2 groups: two comma separated from A 
-# 6. Correl A vs. Correl B: 3 non-empty columns, 4th column correlation [A	B	empty	correlation]
-# 7. Correl delta A vs. Correl delta B: 4 non-empty columns, 3rd column delta, 4th column correlation [A	B	delta	correlation] # needs 4 groups: two comma separated from A and two comma separated from B
-# 8. Correl A vs. Correl B paired: 4 non-empty columns, 3rd column paired, 4th column correlation [A	B	paired	correlation]
-## no need to do paired in 8
-# 9. Timelag Correl A and B (where A and B are different time points): 4 non-empty columns, 3rd column timelag, 4th column correlation [A	B	timelag	correlation]
-## std. correlations: g1T1-g2T1 or g1T2-g2T2
-## calculate timelag correlations for g?T1-g?T2 : will take care of g1T1-g1T2, g1T1-g2T2, etc. 
-## for all gene pairs: give group 1 (T1) to first gene, group 2 (T2) to second gene
-
-
+#==================================================================================================================
+# perform analysis
+#==================================================================================================================
 for(indx in 1:nrow(AnalysToDoList)){
     c = as.vector(AnalysToDoList[indx,])
     
