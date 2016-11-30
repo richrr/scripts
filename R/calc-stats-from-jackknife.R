44a5f85b01f5d7a7bb1ae5aa4c9220d5eb40ecba
Richard Rodrigues <richrr@users.noreply.github.com>
2016-11-30 09:57:11 -0800

    added code to work on comp or correl jack-knifed data

--- R/calc-stats-from-jackknife.R
+++ R/calc-stats-from-jackknife.R
@@ -5,13 +5,24 @@ args = commandArgs(trailingOnly=TRUE)
 path = args[1]
 patt = args[2]
 
+uniq_col_name = "pairName"
+metric_col = "Coefficient"
+
+if(patt == "comp") { 
+    uniq_col_name = "geneName" 
+    metric_col = "FolChMedian"
+}
+
 analys_to_do = unlist(strsplit(args[3], ","))
+
+ofname = paste(c("median-across-jackknife", patt, analys_to_do), collapse='_')
+
 
 files = list.files(path = path, pattern = patt, recursive = T)
 
 # get the pairs from any file
 fi = read.csv(files[1], header=T, check.names=F)
-pairs = as.vector(fi$pairName)
+pairs = as.vector(fi[,uniq_col_name])
 
 
 median_indx <- function(x) {
@@ -31,7 +42,7 @@ median_indx <- function(x) {
 
 if(args[4] == "serial"){
 	final_out_df = data.frame(pairs)
-	colnames(final_out_df) = c("pairName")
+	colnames(final_out_df) = c(uniq_col_name)
 
 	for(a in analys_to_do){
 
@@ -57,11 +68,11 @@ if(args[4] == "serial"){
 		    sorted_df = df[order(df[,pvalCol], na.last = NA),]
 		    #print(nrow(sorted_df))
 
-		    # get the coefficient corresponding to the median p value
+		    # get the coefficient or foldchange corresponding to the median p value
 		    med_indx = median_indx(sorted_df[,pvalCol])
 		    l = med_indx[1]
 		    u = med_indx[2]
-		    coeffCol = grep("Coefficient" , colnames(df), value=T)
+		    coeff_or_fc_col = grep(metric_col , colnames(df), value=T)
 
 		    median_coeff = NA
 		    if( l != u){
@@ -69,27 +80,27 @@ if(args[4] == "serial"){
 			#print(u)
 			v1 = NA
 			v2 = NA
-			if(l > 0) {v1 = sorted_df[,coeffCol][l]} # this could happen when all the vectors in that list were NAs and so we got empty vector in sorted_df
-			if(u > 0) {v2 = sorted_df[,coeffCol][u]}
+			if(l > 0) {v1 = sorted_df[,coeff_or_fc_col][l]} # this could happen when all the vectors in that list were NAs and so we got empty vector in sorted_df
+			if(u > 0) {v2 = sorted_df[,coeff_or_fc_col][u]}
 			#print(v1)
 			#print(v2)
 			median_coeff = (v1+v2)/2.0
 		    } else {
-			median_coeff = sorted_df[,coeffCol][l]
+			median_coeff = sorted_df[,coeff_or_fc_col][l]
 		    }
 		    
-		    # why do we need confidence interval? #print(t.test(sorted_df[,coeffCol]))
+		    # why do we need confidence interval? #print(t.test(sorted_df[,coeff_or_fc_col]))
 		    
 		    out_df = rbind(out_df, data.frame(p , median_pval, median_coeff))
 		    
 		    
 		    
 		}
-		colnames(out_df) = c("pairName", paste(analys, "pvalue", sep='') , paste(analys, "Coefficient", sep=''))
+		colnames(out_df) = c(uniq_col_name, paste(analys, "pvalue", sep='') , paste(analys, metric_col, sep=''))
 		#print(out_df)
 		final_out_df = merge(final_out_df , out_df)
 	}
-	write.csv(final_out_df, "median-across-jackknife.csv", row.names=FALSE, quote=FALSE)
+	write.csv(final_out_df, paste(ofname, ".csv", sep=''), row.names=FALSE, quote=FALSE)
 } else {
 
 	library(foreach)
@@ -100,7 +111,7 @@ if(args[4] == "serial"){
 	registerDoParallel(cl)
 
 	final_out_df = data.frame(pairs)
-	colnames(final_out_df) = c("pairName")
+	colnames(final_out_df) = c(uniq_col_name)
 
 	for(a in analys_to_do){  # Although this can be parallelized, it doesn't save much time since it still might need the pairs to be analyzed sequentially
 
@@ -125,11 +136,11 @@ if(args[4] == "serial"){
 		    sorted_df = df[order(df[,pvalCol], na.last = NA),]
 		    #print(nrow(sorted_df))
 
-		    # get the coefficient corresponding to the median p value
+		    # get the coefficient or foldchange corresponding to the median p value
 		    med_indx = median_indx(sorted_df[,pvalCol])
 		    l = med_indx[1]
 		    u = med_indx[2]
-		    coeffCol = grep("Coefficient" , colnames(df), value=T)
+		    coeff_or_fc_col = grep(metric_col , colnames(df), value=T)
 
 		    median_coeff = NA
 		    if( l != u){
@@ -137,27 +148,27 @@ if(args[4] == "serial"){
 			#print(u)
 			v1 = NA
 			v2 = NA
-			if(l > 0) {v1 = sorted_df[,coeffCol][l]} # this could happen when all the vectors in that list were NAs and so we got empty vector in sorted_df
-			if(u > 0) {v2 = sorted_df[,coeffCol][u]}
+			if(l > 0) {v1 = sorted_df[,coeff_or_fc_col][l]} # this could happen when all the vectors in that list were NAs and so we got empty vector in sorted_df
+			if(u > 0) {v2 = sorted_df[,coeff_or_fc_col][u]}
 			#print(v1)
 			#print(v2)
 			median_coeff = (v1+v2)/2.0
 		    } else {
-			median_coeff = sorted_df[,coeffCol][l]
+			median_coeff = sorted_df[,coeff_or_fc_col][l]
 		    }
 		    
-		    # why do we need confidence interval? #print(t.test(sorted_df[,coeffCol]))
+		    # why do we need confidence interval? #print(t.test(sorted_df[,coeff_or_fc_col]))
 		    
 		    res = data.frame(p , median_pval, median_coeff)
 		    res
 		    
 		}
 
-		colnames(out_df) = c("pairName", paste(analys, "pvalue", sep='') , paste(analys, "Coefficient", sep=''))
+		colnames(out_df) = c(uniq_col_name, paste(analys, "pvalue", sep='') , paste(analys, metric_col, sep=''))
 		#print(out_df)
 		final_out_df = merge(final_out_df , out_df)
 	}
-	write.csv(final_out_df, "median-across-jackknife-parallel.csv", row.names=FALSE, quote=FALSE)
+	write.csv(final_out_df, paste(ofname, "-parallel.csv", sep=''), row.names=FALSE, quote=FALSE)
 
 	#stop cluster
 	stopCluster(cl)
