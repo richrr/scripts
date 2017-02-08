f8303c46c5977b01e1679b44b1c99169c958fcf2
Richard Rodrigues <richrr@users.noreply.github.com>
2017-02-08 10:59:39 -0800

    correctly divides by the colsum instead of the sum of the entire df

--- R/relativize_csv_file.R
+++ R/relativize_csv_file.R
@@ -2,6 +2,8 @@
 # first column has to be ids
 
 
+# http://stackoverflow.com/questions/9447801/dividing-columns-by-colsums-in-r
+
 args = commandArgs(trailingOnly=TRUE)
 #print(length(args))
 
@@ -16,7 +18,8 @@ pow <- function(x=10, y=6) {
 
 relativize = function(infile){
 	df = read.csv(infile, header=T, check.names=F, row.names=1)
-	reldf = df/sum(df)
+	#reldf = df/sum(df) # this is incorrect since it divides by the sum of the matrix
+	reldf = prop.table(as.table(as.matrix(df)), 2)
 	ID = rownames(df)
 	reldf = cbind(ID, reldf)
 	write.csv(reldf, paste(infile, ".rel.csv", sep=''), quote=F, row.names=F)
@@ -25,7 +28,8 @@ relativize = function(infile){
 
 relativize_multply_million = function(infile){
 	df = read.csv(infile, header=T, check.names=F, row.names=1)
-	reldf = df/sum(df)
+	#reldf = df/sum(df) # this is incorrect since it divides by the sum of the matrix
+	reldf = prop.table(as.table(as.matrix(df)), 2)
 	mill_reldf = reldf * pow(10, 6)
 	ID = rownames(df)
 	mill_reldf = cbind(ID, mill_reldf)
